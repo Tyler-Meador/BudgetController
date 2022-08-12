72fd16e1ca6cc2ff54a4a83767b674f7656fe3d0
Tyler Meador <tmeador13@gmail.com>
2022-08-12 13:13:01 -0500

    Initial Commit

---
+++ src/main/java/com/tylermeador/budgetcontroller/BudgetControllerApplication.java
@@ -0,0 +1,13 @@
+package com.tylermeador.budgetcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetControllerApplication.class, args);
+	}
+
+}
